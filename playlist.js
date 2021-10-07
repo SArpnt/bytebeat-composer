5b52e022418e7a7f05139b8747eb322b9a78c5de
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-10-07 18:56:18 -0400

    made playlist parser somewhat more readable

--- playlist.js
+++ playlist.js
@@ -7,13 +7,13 @@
 	function processCodeHTML(code, sRate) {
 		if (typeof code !== 'string')
 			code = code.join('@!@LINE_BREAK@!@');
-		return '<code' + (sRate ? ' samplerate="' + sRate + '"' : '') + '>' +
+		return `<code${sRate ? ` samplerate="${sRate}"` : ``}>${
 			code.replace(/&/g, '&amp;')
 				.replace(/>/g, '&gt;')
 				.replace(/</g, '&lt;')
 				.replace(/ /g, '&#xA0;')
-				.replace(/@!@LINE_BREAK@!@/g, '<br>\r\n') +
-			'</code>';
+				.replace(/@!@LINE_BREAK@!@/g, '<br>\r\n')
+			}</code>`;
 	}
 
 	function getEntryHTML(entry, isChildren) {
@@ -22,40 +22,40 @@
 		let url = entry.url;
 		let authorHTML = '';
 		if (author) {
-			if (typeof author === 'string') {
-				authorHTML = !url ? author : '<a href="' + url + '" target="_blank">' + author + '</a>';
-			} else {
+			if (typeof author === 'string')
+				authorHTML = !url ? author : `<a href="${url}" target="_blank">${author}</a>`;
+			else {
 				let authorsArr = [];
-				for (let j = 0, aLen = author.length; j < aLen; ++j) {
-					authorsArr.push('<a href="' + author[j][1] + '" target="_blank">' + author[j][0] + '</a>');
-				}
+				for (let j = 0, aLen = author.length; j < aLen; ++j)
+					authorsArr.push(`<a href="${author[j][1]}" target="_blank">${author[j][0]}</a>`);
 				authorHTML = authorsArr.join(', ');
 			}
-			authorHTML = descr ? ' (by ' + authorHTML + ')' : 'by ' + authorHTML;
+			authorHTML = descr ? ` (by ${authorHTML})` : `by ${authorHTML}`;
 		}
-		let descrHTML = '';
+		let descrHTML = ``;
 		if (descr)
-			descrHTML = author || !url ? descr : '<a href="' + url + '" target="_blank">' + descr + '</a>';
+			descrHTML = author || !url ? descr : `<a href="${url}" target="_blank">${descr}</a>`;
 		descrHTML += authorHTML;
 		let code = entry.code;
 		let lCode = entry.loadcode;
 		let sRate = entry.samplerate;
-		let starHTML = sRate ? ' <span class="samplerate">' + sRate.substr(0, 2) + 'kHz</span>' : '';
+		let starHTML = sRate ? ` <span class="samplerate">${sRate.substr(0, 2)}kHz</span>` : ``;
 		switch (entry.starred) {
-			case 1: starHTML += ' <span class="star-white"></span>'; break;
-			case 2: starHTML += ' <span class="star-yellow"></span>';
+			case 1: starHTML += ` <span class="star-white"></span>`; break;
+			case 2: starHTML += ` <span class="star-yellow"></span>`;
 		}
-		let descr2HTML = entry.description2 ? ' - ' + entry.description2 : '';
-		let codeLen = !code ? 0 : '<span class="codelength">' +
-			(code instanceof Array ? code.join('').length : code.length) + 'B</span>';
-		let codeHTML = !code && !lCode ? '' :
-			(lCode ? '<a class="code-load" loadcode="' + lCode + '"' +
-				(sRate ? ' samplerate="' + sRate + '"' : '') +
-				'>&#9658; Click to load  pretty code</a>' + (code ? '</br>' : '') : '') +
-			(code ? processCodeHTML(code, sRate) + ' ' + codeLen : '');
+		let descr2HTML = entry.description2 ? ` - ${entry.description2}` : ``;
+		let codeLen = !code ? 0 : `<span class="codelength">${code instanceof Array ? code.join('').length : code.length}B</span>`;
+		let codeHTML = !code && !lCode ? `` :
+			(
+				lCode ? `<a class="code-load" loadcode="${lCode}"${
+					(sRate ? ` samplerate="${sRate}"` : ``)
+				}>&#9658; Click to load pretty code</a>` + (code ? `</br>` : ``) : ``
+			) +
+			(code ? processCodeHTML(code, sRate) + ' ' + codeLen : ``);
 		return isChildren ?
-			codeHTML + (descrHTML ? starHTML + ' ' + descrHTML : starHTML) + descr2HTML :
-			descrHTML + (code || lCode ? starHTML + '<br>\r\n' + codeHTML + descr2HTML : '');
+			`${codeHTML}${descrHTML ? `${starHTML} ${descrHTML}` : starHTML}${descr2HTML}` :
+			`${descrHTML}${code || lCode ? `${starHTML}<br>\r\n${codeHTML}${descr2HTML}` : ``}`;
 	}
 
 	function addPlaylist(obj, id) {
@@ -70,7 +70,7 @@
 					entryHTML.push(getEntryHTML(children[j], true));
 			html.push(entryHTML.join('<br>\r\n'));
 		}
-		$id('library-' + id).innerHTML = '<ul><li>' + html.join('</li>\r\n<li>') + '</li></ul>';
+		$id(`library-${id}`).innerHTML = `<ul><li>${html.join('</li>\r\n<li>')}</li></ul>`;
 	}
 
 	document.addEventListener('DOMContentLoaded', function () {
