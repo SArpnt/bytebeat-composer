bed6774d03602f723f6b364014a475676e3b8af9
SArpnt <36542932+SArpnt@users.noreply.github.com>
2022-01-07 17:55:13 -0500

    working library
    Signed-off-by: SArpnt <36542932+SArpnt@users.noreply.github.com>

--- scripts/library.mjs
+++ scripts/library.mjs
@@ -1,13 +1,17 @@
 import domLoaded from "./domLoaded.mjs";
 
 function parseEntry(entry) {
-	if (Array.isArray(entry.code))
-		entry.code = entry.code.join("\n"); // TODO: check if this is still needed
+	if (Array.isArray(entry.codeOriginal))
+		entry.codeOriginal = entry.codeOriginal.join("\n");
 
 	return entry;
 }
-function stripEntryToSong({ code, sampleRate, mode }, includeCode = false) {
-	if (includeCode)
+function stripEntryToSong(entry, codeType = undefined) {
+	const { sampleRate, mode } = entry;
+	let code = null;
+	if (codeType)
+		code = entry[codeType];
+	if (code)
 		return { code, sampleRate, mode };
 	else
 		return { sampleRate, mode };
@@ -96,14 +100,14 @@ function createEntryElem(entry) {
 
 	if (entry.date) {
 		const dateElem = document.createElement("span");
-		dateElem.classList = "library-song-info";
+		dateElem.className = "library-song-info";
 		dateElem.innerText = `(${entry.date})`;
 
 		entryElem.append(document.createTextNode(" "), dateElem);
 	}
 	if (entry.sampleRate) {
 		const sampleRateElem = document.createElement("span");
-		sampleRateElem.classList = "library-song-info";
+		sampleRateElem.className = "library-song-info";
 		if (entry.sampleRate % 1000 == 0)
 			sampleRateElem.innerText = `${entry.sampleRate.toString().substring(0, entry.sampleRate.length - 3)}kHz`;
 		else
@@ -113,7 +117,7 @@ function createEntryElem(entry) {
 	}
 	if (entry.mode) {
 		const modeElem = document.createElement("span");
-		modeElem.classList = "library-song-info";
+		modeElem.className = "library-song-info";
 		modeElem.innerText = entry.mode;
 
 		entryElem.append(document.createTextNode(" "), modeElem);
@@ -122,40 +126,82 @@ function createEntryElem(entry) {
 	if (entryElem.innerHTML)
 		entryElem.append(document.createElement("br"));
 
-	if (entry.codeFile) {
-		let codeFileElem = document.createElement("a"); // TODO: make this a button
-		codeFileElem.className = "code-load";
-		codeFileElem.innerText = "► Click to load pretty code";
-		codeFileElem.href = "#";
-		const songData = stripEntryToSong(entry);
-		codeFileElem.addEventListener("click", () =>
-			fetch(`library/${entry.codeFile}`, { cache: "no-cache" })
-				.then(response => response.text())
-				.then(code => bytebeat.loadCode(Object.assign(songData, { code })))
-		);
-		entryElem.append(codeFileElem);
-		if (entry.code)
-			entryElem.append(" ");
-	}
-	if (entry.code) {
-		const codeElem = document.createElement("code");
-		codeElem.title = "Click to play this code";
-		codeElem.innerText = entry.code;
-		const fullSongData = stripEntryToSong(entry, true);
-		codeElem.addEventListener("click", () => bytebeat.loadCode(fullSongData));
 
-		const pre = document.createElement("pre");
-		pre.style.margin = "0";
-		pre.style.whiteSpace = "pre-wrap";
-		pre.style.display = "inline";
-		pre.append(codeElem);
-		entryElem.append(pre);
-
-		const codeLengthElem = document.createElement("span");
-		codeLengthElem.classList = "library-song-info";
-		codeLengthElem.innerText = `${entry.code.length}C`;
-		entryElem.append(document.createTextNode(" "), codeLengthElem);
+	if (entry.file) {
+		for (const fileType of [
+			{ name: "formatted", prop: "fileFormatted" },
+			{ name: "minified", prop: "fileMinified" },
+			{ name: "original", prop: "fileOriginal" },
+		]) {
+			if (entry[fileType.prop]) {
+				const codeFileElem = document.createElement("button");
+				codeFileElem.className = "code-load-file";
+				codeFileElem.innerText = `\u25b6 ${fileType.name}`;
+				const songData = stripEntryToSong(entry);
+				codeFileElem.addEventListener("click", () =>
+					fetch(`library/${fileType.name}/${entry.file}`, { cache: "no-cache" })
+						.then(response => response.text())
+						.then(code => bytebeat.loadCode(Object.assign(songData, { code })))
+				);
+				entryElem.append(codeFileElem, " ");
+			}
+		}
+		entryElem.append(document.createElement("br"));
 	}
+
+	{
+		function createCodeTypeElem(name) {
+			const codeTypeElem = document.createElement("span");
+			codeTypeElem.className = `library-song-${name}`;
+
+			const codeElem = document.createElement("code");
+			codeElem.title = "Click to play this code";
+			codeElem.innerText = entry[name];
+			const fullSongData = stripEntryToSong(entry, name);
+			codeElem.addEventListener("click", () => bytebeat.loadCode(fullSongData));
+			codeTypeElem.append(codeElem);
+
+			const codeLengthElem = document.createElement("span");
+			codeLengthElem.className = "library-song-info";
+			codeLengthElem.innerText = `${entry[name].length}C`;
+			codeTypeElem.append(" ", codeLengthElem);
+
+			return codeTypeElem;
+		}
+		let codeOriginalElem = null;
+		if (entry.codeOriginal) {
+			codeOriginalElem = createCodeTypeElem("codeOriginal");
+			entryElem.append(codeOriginalElem);
+		}
+		if (entry.codeMinified) {
+			const codeMinifiedElem = createCodeTypeElem("codeMinified");
+			entryElem.append(codeMinifiedElem);
+
+			const codeTypeToggleElem = document.createElement("button");
+			codeTypeToggleElem.className = "code-type-toggle";
+			codeTypeToggleElem.innerText = "minified";
+
+			if (entry.codeOriginal) {
+				codeOriginalElem.classList.add("disabled");
+				codeTypeToggleElem.addEventListener("click", () => {
+					if (codeTypeToggleElem.innerText === "minified") {
+						codeTypeToggleElem.innerText = "original";
+						codeOriginalElem.classList.remove("disabled");
+						codeMinifiedElem.classList.add("disabled");
+					} else {
+						codeTypeToggleElem.innerText = "minified";
+						codeMinifiedElem.classList.remove("disabled");
+						codeOriginalElem.classList.add("disabled");
+					}
+				});
+			} else {
+				codeTypeToggleElem.disabled = "true";
+			}
+
+			entryElem.append(" ", codeTypeToggleElem);
+		}
+	}
+
 
 	if (entry.children) {
 		let childrenElem = document.createElement("ul");
